26c79fdb60b7535e2abba8c09d37505422496eb0
ack81 <akinney@cloudboltsoftware.com>
2021-04-27 09:33:08 -0700

    simple aws virtual machine

---
+++ aws/variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+  type        = string
+  default     = ""
+  description = "AWS Access Key"
+}
+
+variable "secret_key" {
+  type        = string
+  default     = ""
+  description = "AWS Secret Key"
+}
